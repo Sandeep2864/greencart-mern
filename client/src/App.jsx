c9d19a2182cfbfd47540362bb44b45570f4b595b
sandeep <sr5545434@gmail.com>
2026-03-24 13:22:02 +0530

    now i have added the cart icon in smaller screens and added sample cart view page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import MainBanner from './components/MainBanner'
-import Home from './pages/Home'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import { Toaster } from "react-hot-toast"
-import Footer from './components/Footer'
-import Login from './components/Login'
-import { useAppContext } from './context/AppContext'
-import AllProducts from './pages/AllProducts'
-import ProductCategory from './pages/ProductCategory'
+import React from "react";
+import Navbar from "./components/Navbar";
+import MainBanner from "./components/MainBanner";
+import Home from "./pages/Home";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import { useAppContext } from "./context/AppContext";
+import AllProducts from "./pages/AllProducts";
+import ProductCategory from "./pages/ProductCategory";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
 
 const App = () => {
-
-  const isSellerPath=useLocation().pathname.includes("seller");
-  const {showUserLogin}=useAppContext();
+  const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
 
   return (
-   <div>
-   {isSellerPath ? null: <Navbar/> }
-   {showUserLogin ? <Login/> : null}
-    <Toaster/>
-   <div className={isSellerPath ? " ": "px-6 md:px-16 lg:px-24 xl:px-32"}>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/products' element={<AllProducts/>} />
-       <Route path='/products/:category' element={<ProductCategory/>} />
-    </Routes>
-   </div>
+    <div>
+      {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
+      <Toaster />
+      <div className={isSellerPath ? " " : "px-6 md:px-16 lg:px-24 xl:px-32"}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:category" element={<ProductCategory />} />
+          <Route
+            path="/products/:category/:id"
+            element={<ProductDetails />}/>
+          <Route path="/cart" element={<Cart/>}/>
+        </Routes>
+      </div>
 
-   { !isSellerPath && <Footer/> }
-   </div>
-  )
-}
+      {!isSellerPath && <Footer />}
+    </div>
+  );
+};
 
-export default App
+export default App;
