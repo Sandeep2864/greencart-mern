969e8703630e22510d971078e7bac834207398aa
sandeep <sr5545434@gmail.com>
2026-03-30 11:35:32 +0530

    little change in server.js with simple api test and start server on port 4000

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const PORT = 4000;
+
+app.get('/',(req,res) => res.send('Api is Working'))
+
+app.listen(PORT, () => {
+   console.log(`Server is running on http://locahost:${PORT}`)
+})
+
